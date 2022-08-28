163340f9b6798e17a31418e4db6ef70e780f670a
Ben O'Neill <ben@benoneill.xyz>
2022-08-28 19:39:26 -0400

    progress

--- chip8/chip8.c
+++ chip8/chip8.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
@@ -11,9 +10,13 @@
 #define WINDOW_SCALE_X DEFAULT_WINDOW_WIDTH / DISPLAY_WIDTH
 #define WINDOW_SCALE_Y DEFAULT_WINDOW_HEIGHT / DISPLAY_HEIGHT
 
+int display[DISPLAY_WIDTH][DISPLAY_HEIGHT];
+int running = 0;
+
 uint8_t mem[0xFFF];
-bool display[DISPLAY_WIDTH][DISPLAY_HEIGHT];
-bool running = false;
+uint8_t V[16];
+uint16_t pc = 0x200;
+uint16_t sp = 0, dt = 0, st = 0;
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -30,18 +33,12 @@ SDL_Rect winRect = {
 	.h = DEFAULT_WINDOW_HEIGHT,
 };
 
-void
-cls(void)
+int
+check_carry(int x, int y)
 {
-	for (int i = 0; i < DISPLAY_WIDTH; i++) {
-		for (int j = 0; j < DISPLAY_HEIGHT; j++) {
-			display[i][j] = false;
-		}
-	}
+	return (x + y) > UINT8_MAX;
 }
 
-/**
-  */
 void
 draw(int x, int y)
 {
@@ -66,6 +63,91 @@ render(void)
 	SDL_RenderPresent(renderer);
 }
 
+void
+parse_instruction(uint16_t in)
+{
+	int x = (in & 0x0F00) >> 8;
+	int kk = in & 0x00FF;
+	int y = (in & 0x00F0) >> 4;
+	int nnn = in & 0x0FFF;
+
+	int a = (in & 0xF000) >> 12;
+	int b = in & 0x000F;
+	switch (a) {
+		case 0x0:
+			if (in == 0x00E0) {
+				/* CLS */
+				for (int i = 0; i < DISPLAY_WIDTH; i++) {
+					for (int j = 0; j < DISPLAY_HEIGHT; j++) {
+						display[i][j] = false;
+					}
+				}
+			} else if (in == 0x00EE) {
+				/* RET */
+				pc = mem[sp];
+				sp -= 1;
+			}
+			break;
+		case 0x1:
+			/* JP addr */
+			pc = nnn;
+			break;
+		case 0x2:
+			/* CALL addr */
+			sp++;
+			mem[sp] = pc;
+			pc = nnn;
+			break;
+		case 0x3:
+			/* SE Vx, byte */
+			if (V[x] == kk)
+				pc += 2;
+			break;
+		case 0x4:
+			/* SNE Vx, byte */
+			if (V[x] != kk)
+				pc += 2;
+			break;
+		case 0x5:
+			/* SE Vx, Vy */
+			if (V[x] == V[y])
+				pc += 2;
+			break;
+		case 0x6:
+			/* LD Vx, byte */
+			V[x] = kk;
+			break;
+		case 0x7:
+			/* ADD Vx, byte */
+			V[x] += kk;
+			break;
+		case 0x8:
+			switch (b) {
+				case 0x0:
+					/* LD Vx, Vy */
+					V[x] = V[y];
+					break;
+				case 0x1:
+					/* OR Vx, Vy */
+					V[x] = V[x] | V[y];
+					break;
+				case 0x2:
+					/* AND Vx, Vy */
+					V[x] = V[x] & V[y];
+					break;
+				case 0x3:
+					/* XOR Vx, Vy */
+					V[x] = V[x] ^ V[y];
+					break;
+				case 0x4:
+					/* ADD Vx, Vy */
+					V[0xF] = check_carry(V[x], V[y]);
+					V[x] += V[y];
+					break;
+			}
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -79,7 +161,8 @@ main(int argc, char *argv[])
 	render();
 
 	SDL_Event e;
-	running = true;
+
+	running = 1;
 	while (running) {
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
