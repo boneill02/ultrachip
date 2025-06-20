79a3a55cd153cea0c17d20f7e1cfdb8b7f2025b5
Ben O'Neill <ben@oneill.sh>
2025-06-20 11:16:22 -0400

    Define load_quirks in test_debug

--- test/c8/test_debug.c
+++ test/c8/test_debug.c
@@ -5,11 +5,37 @@
 #include "util/exception.c"
 #include "util/util.c"
 #include "c8/debug.c"
+#include "c8/font.c"
 
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 
+void load_quirks(int *flags, const char *s) {
+	NULLCHECK2(flags, s);
+
+	for (int i = 0; i < strlen(s); i++) {
+		switch (s[i]) {
+			case 'b':
+				*flags ^= FLAG_QUIRK_BITWISE;
+				break;
+			case 'd':
+				*flags ^= FLAG_QUIRK_DRAW;
+				break;
+			case 'j':
+				*flags ^= FLAG_QUIRK_JUMP;
+				break;
+			case 'l':
+				*flags ^= FLAG_QUIRK_LOADSTORE;
+				break;
+			case 's':
+				*flags ^= FLAG_QUIRK_SHIFT;
+				break;
+			default:
+				safe_exit(INVALID_QUIRK_EXCEPTION);
+		}
+	}
+}
 void setUp(void) {
 	srand(time(NULL));
 }
