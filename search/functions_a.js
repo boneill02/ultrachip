c915d1a59c1331bfc3b68be1f7624c5cb09ed2fc
boneill02 <benoneill617@gmail.com>
2025-07-04 17:49:20 +0000

    Deploying to gh-pages from @ boneill02/libc8@3d1ead4abb650a1a30f01925dd1980a86013d293 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['substitute_5flabels_0',['substitute_labels',['../symbol_8c.html#a59e8950057001a247f2803a596018a87',1,'substitute_labels(symbol_list_t *symbols, label_list_t *labels):&#160;symbol.c'],['../symbol_8h.html#abfbba5228c1d10a3a51e29e663fd2b26',1,'substitute_labels(symbol_list_t *, label_list_t *):&#160;symbol.c']]]
+  ['shift_0',['shift',['../symbol_8c.html#ae26429d2a6d6e7c4cca9f7ec8a7c3298',1,'shift(uint16_t fmt):&#160;symbol.c'],['../symbol_8h.html#a944d91df6b3cf432030bfe5a188e9c22',1,'shift(uint16_t):&#160;symbol.c']]],
+  ['substitute_5flabels_1',['substitute_labels',['../symbol_8c.html#a59e8950057001a247f2803a596018a87',1,'substitute_labels(symbol_list_t *symbols, label_list_t *labels):&#160;symbol.c'],['../symbol_8h.html#abfbba5228c1d10a3a51e29e663fd2b26',1,'substitute_labels(symbol_list_t *, label_list_t *):&#160;symbol.c']]]
 ];
